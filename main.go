bb1fb8e57071303be4676394f189b3e7a561498b
isongjosiah <isongjosiah@gmail.com>
2020-06-24 21:05:12 +0100

    Initial commit

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-
-	parser "github.com/isongjosiah/Hack-VM-Translator/parser"
 )
 
 func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("Usage main.go <file.vm>")
 	}
-	parser.Construct()
+
 }
